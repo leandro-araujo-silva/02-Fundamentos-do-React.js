877bbb9bfe362fa54221e74c15fe376bc69d824e
Leandro Araújo <leandro.arauj047.edu@gmail.com>
2022-06-09 10:20:59 -0300

    Adicionando

--- rede-social/src/App.jsx
+++ rede-social/src/App.jsx
@@ -1,9 +1,14 @@
+import { Header } from './components/Header'
 import {Post} from './Post'
+
+import './global.css'
 
 export function App() {
 
   return (
     <div>
+      <Header />
+
       <Post 
         author="Leandro" 
         content="Na informática, o jogo online é um tipo de jogo eletrônico jogados via Internet."
