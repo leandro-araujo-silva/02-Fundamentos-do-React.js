f10d65fc494484893bb8446d364f58c23e067f73
Leandro Araújo <leandro.arauj047.edu@gmail.com>
2022-06-11 10:10:15 -0300

    Adicionando

--- rede-social/src/App.jsx
+++ rede-social/src/App.jsx
@@ -1,10 +1,10 @@
 import { Header } from './components/Header'
-import {Post} from './Post'
+import {Post} from './components/Post'
+import { Sidebar } from './components/Sidebar'
 
 import styles from './App.module.css'
 
 import './global.css'
-import { Sidebar } from './components/Sidebar'
 
 export function App() {
 
@@ -15,7 +15,7 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post author="Leandro Araújo" content="Lorem ipsum dolor sit amet consectetur adipisicing elit."/>
+          <Post author="Diego Fernandes" content="Lorem ipsum dolor sit amet consectetur adipisicing elit."/>
           <Post author="Diego Fernandes" content="Lorem ipsum dolor sit amet consectetur adipisicing elit."/>
         </main>
       </div>
