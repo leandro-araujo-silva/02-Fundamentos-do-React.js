b0ef4d82c906fdfb56eac4b0933bb80f7ec7a5f0
Leandro Araújo <leandro.arauj047.edu@gmail.com>
2022-06-24 16:22:19 -0300

    Adicionando

--- rede-social/src/components/Comment.jsx
+++ rede-social/src/components/Comment.jsx
@@ -2,7 +2,11 @@ import { ThumbsUp, Trash } from 'phosphor-react'
 import { Avatar } from './Avatar'
 import styles from './Comment.module.css'
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+  function handleDeleteComment() {
+    onDeleteComment(content)
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/2254731?v=4" />
@@ -15,7 +19,7 @@ export function Comment({ content }) {
               <time title="11 de Maio às 08:13h" dateTime='2022-05-11 00:13:30'>Cerca de 1h atrás</time>
             </div>
 
-            <button title="Deletar comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={24}/>
             </button>
           </header>
--- rede-social/src/components/Post.jsx
+++ rede-social/src/components/Post.jsx
@@ -24,6 +24,10 @@ export function Post({ author, publishedAt, content }) {
     setNewCommentText(event.target.value)
   }
   
+  function deleteComment(comment) {
+    console.log(`Deletar comentário ${comment}`)
+  }
+
   return (
     <article className={styles.post}>
       <header>
@@ -42,9 +46,9 @@ export function Post({ author, publishedAt, content }) {
         {
           content.map(line => {
             if(line.type === 'paragraph') {
-              return <p>{line.content}</p>
+              return <p key={line.content}>{line.content}</p>
             } else if (line.type === 'link') {
-              return <p><a href="a">{line.content}</a></p>
+              return <p key={line.content}><a href="a">{line.content}</a></p>
             }
           })
         }
@@ -68,7 +72,12 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map(comment => {
-          return <Comment content={comment} />
+          return (
+            <Comment 
+              key={comment} 
+              content={comment} onDeleteComment={deleteComment} 
+            />
+            )
         })}
       </div>
     </article>
